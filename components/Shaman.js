e9b04d9c4f1514fe3fc7a909e6131518498b3628
Miika Kuisma <miika@moreyes.fi>
2019-01-23 21:27:27 +0200

    fixed looped audio endings

--- components/Shaman.js
+++ components/Shaman.js
@@ -34,9 +34,9 @@ export function playShaman () {
 }
 
 export async function endShaman () {
+  shaman[0].audioObject.stopAsync()
+  shaman[1].audioObject.playAsync()
   playing = false
-  await shaman[0].audioObject.stopAsync()
-  await shaman[1].audioObject.playAsync()
 }
 
 async function triggerSound (sound) {
